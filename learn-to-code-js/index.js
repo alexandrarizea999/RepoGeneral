5116d5d792b35e348f0b7382c623d3e1a930e850
alexandrarizea999 <alexandrarizea222@yahoo.com>
2024-11-18 00:04:50 +0200

    17.11: leart-to-code-js updates

---
+++ learn-to-code-js/index.js
@@ -0,0 +1,5 @@
+number1 = parseInt(prompt("Introduceti primul numar:", 0))
+number2 = parseInt(prompt("Introduceti al doilea numar:", 0))
+
+suma = number1 + number2
+alert("Suma celor doua numere este = " + suma)
