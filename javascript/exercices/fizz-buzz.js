b0f606117c46a0d103fa635751a5e31fd7147d53
alexandrarizea999 <alexadrarizea222@yahoo.com>
2025-01-20 17:50:01 +0200

    20/02 objects + arrays

--- javascript/exercices/fizz-buzz.js
+++ javascript/exercices/fizz-buzz.js
@@ -6,6 +6,8 @@ function fizzBuzz(number){
     } else if(number % 5 === 0){
         console.log('Buzz');
     } else console.log(number); 
+
+    // (number % 3 === 0 && number % 5 === 0) ? console.log('FizzBuzz') : console.log('Buzz')
 }
 
 fizzBuzz(18);
