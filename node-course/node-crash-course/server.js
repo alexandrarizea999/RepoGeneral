2fe3a2a62fdc3d341fa5e5fa0ea7f80fce5b32a0
alexandrarizea999 <alexadrarizea222@yahoo.com>
2025-01-26 10:30:43 +0200

    get post

--- node-course/node-crash-course/server.js
+++ node-course/node-crash-course/server.js
@@ -10,8 +10,29 @@ const server = http.createServer((req, res) => {
     // res.statusCode = 404
     // res.end('<h1>Hello World</h1>')
 
-    res.writeHead(200, { 'Content-type': 'text/html' })
-    res.end('<h1>Hello world!</h1>')
+    // console.log(req.url)
+    // console.log(req.method)
+
+    try {
+        // Check if GET request
+        if(req.method === 'GET'){
+            if(req.url === '/'){
+                res.writeHead(200, { 'Content-type': 'text/html' })
+                res.end('<h1>HomePage</h1>')
+            }else if(req.url === '/about'){
+                res.writeHead(200, { 'Content-type': 'text/html' })
+                res.end('<h1>About</h1>')
+            }else{
+                res.writeHead(404, { 'Content-type': 'text/html' })
+                res.end('<h1>Page not found.</h1>')
+            }
+        }else{
+            throw new Error('Method now allowed.');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-type': 'text/html' })
+        res.end('<h1>Server error.</h1>')
+    }
 })
 
 server.listen(PORT, () => {
